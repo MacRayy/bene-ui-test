0338fab5c7812c6aaa71ba0e6616152b91abffdd
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-05-12 19:48:05 +0200

    feat(app): save the inputs

--- task-2/multi-inputs/src/App.js
+++ task-2/multi-inputs/src/App.js
@@ -7,41 +7,47 @@ class App extends Component {
 		this.state = {
 			inputFields: [
 				'first',
-				'second'
+				'second',
+				''
 			]
 		}
 	}
 
 
-	addNewInput(event) {
-		this.setState({
-			inputFields: [...this.state.inputFields, '']
-		})
+	addNewInput() {
+		this.setState({ inputFields: [...this.state.inputFields, ''] })
 		console.log(this.state)
+	}
+
+	saveInputs() {
+		const inputValues = Array.from(document.querySelectorAll('.input')).map((field, index) => {
+			return field.value
+		})
+		this.setState({ inputFields: inputValues })
+
+		console.info(inputValues)
+		console.info(this.state)
 	}
 
 	render() {
 		return (
 			<div className="App">
-				<main>
-					<h1>Test</h1>
-					<form className="form">
-						{
-							this.state.inputFields.map((input, index) => {
-								return (
-									<div key={index}>
-										<label>test attribute</label>
-										<input className={`input-${index}`} type="text" name="input" onClick={this.addNewInput.bind(this)} defaultValue={input}/>
-									</div>
-								)
-							})
-						}
-					</form>
+				<h1>Test</h1>
+				<main className="input-container">
+					{
+						this.state.inputFields.map((input, index) => {
+							return (
+								<div key={index}>
+									<label>test attribute</label>
+									<input className={`input input-${index}`} type="text" name="input" onClick={this.addNewInput.bind(this)} defaultValue={input}/>
+								</div>
+							)
+						})
+					}
 				</main>
-
 				<aside>
 					<button>cancel</button>
-					<button>save</button>
+					<button onClick={this.saveInputs.bind(this)}>save</button>
 				</aside>
 			</div>
 		)
