540323a358c3fbf81ac807fd19ffbd662c4a94f3
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-05-12 17:48:10 +0200

    feat(app): render the input fields from state

--- task-2/multi-inputs/src/App.js
+++ task-2/multi-inputs/src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	constructor() {
+		super()
+		this.state = {
+			inputFields: [
+				'first',
+				'second'
+			]
+		}
+	}
+
+
+	addNewInput() {
+		console.log('alma')
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<main>
+					<h1>Test</h1>
+					<form className="form">
+						{
+							this.state.inputFields.map((input, index) => {
+								return (
+									<div key={index}>
+										<label>test attribute</label>
+										<input type="text" name="first" onClick={this.addNewInput} defaultValue={input}/>
+									</div>
+								)
+							})
+						}
+					</form>
+				</main>
+
+				<aside>
+					<button>cancel</button>
+					<button>save</button>
+				</aside>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
